d5f4b7a34d2b8811fbc07368bbbeb9c7af11c76f
Ricky Saechao <ricky@launchbadge.com>
2025-02-08 00:10:47 -0800

    chore: replace Hedera with Hiero name
    Signed-off-by: Ricky Saechao <ricky@launchbadge.com>

--- Tests/HieroE2ETests/Schedule/ScheduleSign.swift
+++ Tests/HieroE2ETests/Schedule/ScheduleSign.swift
@@ -2,7 +2,7 @@
  * ‌
  * Hedera Swift SDK
  * ​
- * Copyright (C) 2022 - 2024 Hedera Hashgraph, LLC
+ * Copyright (C) 2022 - 2025 Hiero LLC
  * ​
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,7 +13,7 @@
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permiss  ions and
+ * See the License for the specific language governing permissions and
  * limitations under the License.
  * ‍
  */
