cc68eb829314550bc2cc2ed0f1817bb30f69c39a
Rob Walworth <robert.walworth@swirldslabs.com>
2025-10-22 15:20:16 -0400

    ci: work work work work work - Rihanna
    Signed-off-by: Rob Walworth <robert.walworth@swirldslabs.com>

--- Sources/Hiero/Crypto/CryptoSha2.swift
+++ Sources/Hiero/Crypto/CryptoSha2.swift
@@ -1,7 +1,14 @@
 // SPDX-License-Identifier: Apache-2.0
 
-import CryptoKit
 import Foundation
+
+#if canImport(Crypto)
+// Prefer Swift Crypto (cross-platform)
+import Crypto
+#elseif canImport(CryptoKit)
+// Fallback to Apple CryptoKit (Darwin only)
+import CryptoKit
+#endif
 
 extension Crypto {
     internal enum Sha2 {
@@ -9,6 +16,7 @@ extension Crypto {
         case sha384
         case sha512
 
+        @inline(__always)
         internal static func digest(_ kind: Sha2, _ data: Data) -> Data {
             kind.digest(data)
         }
@@ -16,30 +24,41 @@ extension Crypto {
         internal func digest(_ data: Data) -> Data {
             switch self {
             case .sha256:
+                #if canImport(Crypto)
+                return Data(SHA256.hash(data: data))
+                #else
                 return Data(CryptoKit.SHA256.hash(data: data))
+                #endif
+
             case .sha384:
+                #if canImport(Crypto)
+                return Data(SHA384.hash(data: data))
+                #else
                 return Data(CryptoKit.SHA384.hash(data: data))
+                #endif
+
             case .sha512:
+                #if canImport(Crypto)
+                return Data(SHA512.hash(data: data))
+                #else
                 return Data(CryptoKit.SHA512.hash(data: data))
+                #endif
             }
         }
 
         /// Hash data using the `sha256` algorithm.
-        ///
-        /// - Parameter data: the data to be hashed.
-        ///
-        /// - Returns: the hash of `data`.
         internal static func sha256(_ data: Data) -> Data {
             digest(.sha256, data)
         }
 
         /// Hash data using the `sha384` algorithm.
-        ///
-        /// - Parameter data: the data to be hashed.
-        ///
-        /// - Returns: the hash of `data`.
         internal static func sha384(_ data: Data) -> Data {
             digest(.sha384, data)
+        }
+
+        /// Hash data using the `sha512` algorithm.
+        internal static func sha512(_ data: Data) -> Data {
+            digest(.sha512, data)
         }
     }
 }
