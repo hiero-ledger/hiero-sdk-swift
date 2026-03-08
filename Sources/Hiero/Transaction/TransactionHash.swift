c3fea3d2d65fd77d591155d50839afec3ca5f6de
freya-docs <freya.docs.pp@gmail.com>
2026-03-08 22:31:56 +0100

    Update TransactionHash.swift
    Signed-off-by: freya-docs <freya.docs.pp@gmail.com>

--- Sources/Hiero/Transaction/TransactionHash.swift
+++ Sources/Hiero/Transaction/TransactionHash.swift
@@ -2,17 +2,13 @@
 
 import Foundation
 
-/// The public struct that represents the SHA-384 hash of a transaction. It is used to uniquely identify the transaction.
 public struct TransactionHash: CustomStringConvertible {
-    /// Computes a SHA-384 cryptographic hash of the input data.
     internal init(hashing data: Data) {
         self.data = Sha2.sha384(data)
     }
 
-    /// Stores the raw hash value.
     public let data: Data
 
-    /// Returns the hash value converted to a hexadecimal string.
     public var description: String {
         data.hexStringEncoded()
     }
