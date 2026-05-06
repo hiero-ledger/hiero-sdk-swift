ae415124654052ecb3305d6410fce70eada6bc12
Rob Walworth <robert.walworth@swirldslabs.com>
2026-05-06 14:12:11 -0400

    chore: release v0.51.0
    Signed-off-by: Rob Walworth <robert.walworth@swirldslabs.com>

--- Sources/Hiero/Version.swift
+++ Sources/Hiero/Version.swift
@@ -3,5 +3,5 @@
 /// Contains SDK version information.
 public enum VersionInfo {
     /// The current version of the Hiero SDK.
-    public static let version = "0.50.0-dev"
+    public static let version = "0.51.0"
 }
